6ffdd3e5fe36b2db9ec2927253a893721249fd05
Rodney.CA <58489167+rodhino212@users.noreply.github.com>
2020-10-16 19:02:42 +0000

    Etape 06

--- src/main/java/ManagerBean.java
+++ src/main/java/ManagerBean.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Array;
 import java.time.Year;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 
@@ -7,9 +11,22 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name="manager")
 public class ManagerBean{
 
-    private String version = "1.0-SNAPSHOT";
+	private String version = "1.0-SNAPSHOT";
 
-	public ArrayList<Etudiant> studentList;
+	private List<Etudiant> studentList = Arrays.asList(
+		new Etudiant[]{
+		new Etudiant(0,"Léo","Freddy","0696445232","01/12/1998"),
+		new Etudiant(1,"Dilio","Medhy","0696324587","04/11/1997"),
+		new Etudiant(2,"Gilgan","Therry","0696458526","01/12/1999"),
+		new Etudiant(3,"Matthieu","Vickcy","0696354789","10/05/1998"),
+		new Etudiant(4,"Thierney","Dilan","0696654819","27/02/1998")
+		});
+
+	public List<Etudiant>getStudentList(){
+		return studentList;
+	}
+	
+	/*public ArrayList<Etudiant> studentList;
 	
 	public ArrayList<Etudiant>getStudentList(){
 
@@ -22,9 +39,24 @@ public class ManagerBean{
 
 		return studentList;
 
+	}*/
+
+	public List<Formation> formationList = Arrays.asList(
+	new Formation[]{
+		new Formation(0,"Licence COM","2 ans"),
+		new Formation(1,"Licence math","3 ans"),
+		new Formation(2,"Licence de lettres","4 ans"),
+		new Formation(3,"Licence info","1 ans"),
+		new Formation(4,"Licence langue","2 ans")
+
+	});
+
+	public List<Formation>getformationList(){
+
+		return formationList;
 	}
 
-	public ArrayList<Formation> formationList;
+	/*public ArrayList<Formation> formationList;
 	
 	public ArrayList<Formation>getFormationList(){
 
@@ -33,33 +65,52 @@ public class ManagerBean{
 		formationList.add(new Formation(1,"Licence math","3 ans"));
 		formationList.add(new Formation(2,"Licence de lettres","4 ans"));
 		formationList.add(new Formation(3,"Licence info","1 ans"));
+		formationList.add(new Formation(4,"Licence langue","2 ans"));
 
 		return formationList;
 
-	}
+	}*/
+
 
 	public Year y = Year.now();
 	public Year t = Year.of(2017);
 	public Year f = Year.of(2018);
 	public Year d = Year.of(2019);
-	public ArrayList<Promotion> promotionList;
+
+/*	public ArrayList<Promotion> promotionList;
 
 	public ArrayList<Promotion>getPromotionList(){
+			//String o = formationList.get(0).getName();
+
 		promotionList = new ArrayList<>();
-		promotionList.add(new Promotion(y,"Licence COM","Jennifer"));
+		promotionList.add(new Promotion(y,"Licence COM","Freddy"));
 		promotionList.add(new Promotion(t,"Licence math","Jeanne"));
 		promotionList.add(new Promotion(f,"Licence de lettres","Chris"));
-		promotionList.add(new Promotion(d,"Licence info","Paulo"));
-
+		promotionList.add(new Promotion(d,"Licence info","Paulo")); 
 		return promotionList;
+	}*/
 
+	private List<Promotion> promotionList = Arrays.asList(
+		new Promotion[] {
+			new Promotion (y,formationList.get(0).getName(),studentList.get(0).getFirstName()),
+			new Promotion (d,formationList.get(1).getName(),studentList.get(1).getFirstName()),
+			new Promotion (f,formationList.get(2).getName(),studentList.get(2).getFirstName()),
+			new Promotion (d,formationList.get(3).getName(),studentList.get(3).getFirstName()),
+			new Promotion (t,formationList.get(4).getName(),studentList.get(4).getFirstName())
+
+		}
+
+	);
+
+	public List<Promotion>getpromotionList(){
+		return promotionList;
 	}
 
     public String getVersion() {
         return version;
-    }
-
+	}
+	
 /*	*/
 
-    
 }
+
